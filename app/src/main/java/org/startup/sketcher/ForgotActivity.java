70acc09722425a733894846430cd3fd4ce03cb3b
Yasuo <yasuodev@protonmail.com>
2017-05-17 12:33:42 +0900

    Complete forgot password function

--- app/src/main/java/org/startup/sketcher/ForgotActivity.java
+++ app/src/main/java/org/startup/sketcher/ForgotActivity.java
@@ -1,20 +1,26 @@
 package org.startup.sketcher;
 
-
 import android.app.ProgressDialog;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.speech.tts.Voice;
+import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.protocol.HTTP;
+import org.apache.http.util.EntityUtils;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.startup.sketcher.util.Constant;
 import org.startup.sketcher.util.Util;
-
-import java.util.List;
 
 import butterknife.InjectView;
 import butterknife.OnClick;
@@ -24,6 +30,16 @@ public class ForgotActivity extends BaseActivity {
 
     @InjectView(R.id.etEmail)
     EditText etEmail;
+    @InjectView(R.id.tvTitle)
+    TextView tvTitle;
+    @InjectView(R.id.viewSuccess)
+    LinearLayout viewSuccess;
+    @InjectView(R.id.tvSuccess)
+    TextView tvSuccess;
+    @InjectView(R.id.btnReset)
+    View btnReset;
+    @InjectView(R.id.btnBackToLogin)
+    View btnBackToLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,18 +57,23 @@ public class ForgotActivity extends BaseActivity {
             toast("Please enter valid email.");
         } else {
             if (Util.isOnline(getApplicationContext())) {
-                new ForgotPassword(email).execute();
+                (new ForgotPassword(email)).execute();
             } else {
                 toast(Constant.network_error);
             }
         }
     }
 
+    @OnClick(R.id.btnBackToLogin)
+    public void onBack(View view) {
+        finish();
+    }
+
     class ForgotPassword extends AsyncTask<Void, String, String> {
 
         String email;
+        String responseString;
         ProgressDialog progressDialog;
-        String response;
 
         public ForgotPassword(String email) {
             this.email = email;
@@ -61,24 +82,41 @@ public class ForgotActivity extends BaseActivity {
         @Override
         public void onPreExecute() {
             super.onPreExecute();
-            progressDialog = new ProgressDialog(ForgotActivity.this, R.style.MyTheme);
-            progressDialog.setCancelable(false);
-            progressDialog.setProgressStyle(android.R.style.Widget_ProgressBar_Small);
-            progressDialog.show();
+
+            if (progressDialog == null)
+                progressDialog = ProgressDialog.show(ForgotActivity.this, null, null, true, false);
         }
 
         @Override
         protected String doInBackground(Void... params) {
+
+            String envelope = "<soapenv:Envelope xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:urn=\"urn:quote5\">"+
+                    "<soapenv:Header/>"+
+                    "<soapenv:Body>"+
+                    "<urn:forgotPassword soapenv:encodingStyle=\"http://schemas.xmlsoap.org/soap/encoding/\">"+
+                    "<email xsi:type=\"xsd:string\">%s</email>"+
+                    "</urn:forgotPassword>"+
+                    "</soapenv:Body>"+
+                    "</soapenv:Envelope>";
+
+            String soapmessage = String.format(envelope, email);
+
+            HttpPost httpPost = new HttpPost(Constant.URL);
+            StringEntity entity;
+
             try {
-                JSONObject jData = new JSONObject();
-                jData.put("email", email);
-
-
-            } catch (JSONException e) {
+                entity = new StringEntity(soapmessage, HTTP.UTF_8);
+                httpPost.setHeader("Content-Type", "text/xml;charset=UTF-8");
+                httpPost.setEntity(entity);
+                HttpClient client = new DefaultHttpClient();
+                HttpResponse response = client.execute(httpPost);
+                responseString = EntityUtils.toString(response.getEntity());
+                Log.d("request: ", responseString);
+            } catch (Exception e) {
                 e.printStackTrace();
             }
 
-            return response;
+            return responseString;
         }
 
         @Override
@@ -91,12 +129,23 @@ public class ForgotActivity extends BaseActivity {
 
             try {
                 JSONObject jObj = new JSONObject(result);
-                int status = jObj.optInt("success");
+                boolean status = jObj.optBoolean("Status");
 
-                toast(jObj.optString("msg"));
-                if (status == 1) {
-                    finish();
+                if (status) {
+                    tvTitle.setVisibility(View.GONE);
+                    viewSuccess.setVisibility(View.VISIBLE);
+                    tvSuccess.setText("An e-mail has been sent to "+email+" with further instructions.");
+
+                    etEmail.setVisibility(View.GONE);
+                    btnReset.setVisibility(View.GONE);
+                    btnBackToLogin.setVisibility(View.VISIBLE);
+
+                } else {
+                    etEmail.setBackgroundResource(R.drawable.textviewredborder);
+                    String message = jObj.optString("Message");
+                    toast(message);
                 }
+
             } catch (JSONException e) {
                 e.printStackTrace();
             }
